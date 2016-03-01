e88fafd6264a57023723d010a45d17c16b1a8596
Carlos Alcala <carlos.alcala@icloud.com>
2016-03-01 19:11:40 -0400

    Review/Test RNCryptor Create Base Swift Project with required libraries Implemented UI layout and base ViewControllers to handle the functionality required

---
+++ EncryptApp/EncryptApp/Classes/Managers/NetworkManager/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  EncryptApp
+//
+//  Created by Carlos Alcala on 3/1/16.
+//  Copyright (c) 2016 Carlos Alcala. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+   
+}
---
+++ EncryptApp/EncryptApp/Support/BridgeHeader.h
@@ -0,0 +1,90 @@
+//
+//  BridgeHeader.h
+//  Juana La Iguana
+//
+//  Created by Reyneiro Hernandez on 12/16/14.
+//  Copyright (c) 2014 Reyneiro Hernandez. All rights reserved.
+//
+
+
+//#import <ReactiveCocoa/RACEXTScope.h>
+//#import <ReactiveCocoa/ReactiveCocoa.h>
+//#import <RestKit/CoreData/RKEntityMapping.h>
+//#import <MobileCoreServices/MobileCoreServices.h>
+//#import <SystemConfiguration/SystemConfiguration.h>
+//#import <CoreData/CoreData.h>
+//#import <RestKit/RestKit.h>
+//#import <RestKit/RestKit.h>
+//#import <RestKit/CoreData.h>
+//#import <CoreData/CoreData.h>
+//#import <RestKit/ObjectMapping.h>
+//
+////#import <RestKit/CoreData.h>
+////#import <RestKit/ObjectMapping.h>
+//#import <RestKit/CoreData/RKCoreData.h>
+//#import <RestKit/CoreData/RKEntityMapping.h>
+//
+//
+//#import <AFNetworking/AFNetworking.h>
+//#import <AFOAuth2Client/AFOAuth2Client.h>
+
+
+#import <ReactiveCocoa/RACEXTScope.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+#import <RestKit/CoreData/RKEntityMapping.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <CoreData/CoreData.h>
+#import <RestKit/RestKit.h>
+
+#import <RestKit/RestKit.h>
+#import <RestKit/CoreData.h>
+#import <CoreData/CoreData.h>
+#import <RestKit/ObjectMapping.h>
+
+#import <RestKit/CoreData/RKCoreData.h>
+#import <RestKit/CoreData/RKEntityMapping.h>
+#import "Restkit+Swift.h"
+
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/SDWebImagePrefetcher.h>
+
+#import <AFNetworking/AFNetworking.h>
+#import <AFOAuth2Client/AFOAuth2Client.h>
+
+#import "UIAppearance+Swift.h"
+
+#import "BaseObject.h"
+#import "Interaction.h"
+#import "InfoInstagram.h"
+#import "InfoTwitter.h"
+#import "InfoFacebook.h"
+#import "Venue.h"
+#import "Activity.h"
+#import "ActivityOwner.h"
+#import "VenueUser.h"
+#import "MutedChannel.h"
+#import "Identity.h"
+#import "Media.h"
+#import "EngageAction.h"
+#import "Alert.h"
+#import "Comment.h"
+#import "NetworkUser.h"
+#import "AlertCondition.h"
+#import "Location.h"
+#import "Filter.h"
+#import "SWRevealTableViewCell.h"
+#import "VideoTwitter.h"
+#import "MBProgressHUD.h"
+#import "EngageCategory.h"
+#import "CompanyInfo.h"
+#import "PeopleStats.h"
+#import "PostStats.h"
+#import "Language.h"
+#import "ReactionGroup.h"
+#import "Action.h"
+#import "EngageActionInfo.h"
+#import "ReactionActionInfo.h"
+#import "ActionInfo.h"
+
